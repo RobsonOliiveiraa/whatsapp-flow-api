f03b58b92f8ce421b135da6006a698ff1569310a
RobsonOliiveiraa <robinhoramos19@gmail.com>
2025-05-03 03:54:16 +0000

    add keys

--- src/controllers/flowController.js
+++ src/controllers/flowController.js
@@ -2,9 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const verifySignature = require('../utils/verifySignature');
 
-let publicKey = ''; // Variável para armazenar a chave pública
+let publicKey = ''; // Variável para armazenar a chave pública em memória
 
-// Função para upload da chave pública
 const uploadPublicKey = (req, res) => {
   const { key } = req.body;
 
@@ -12,25 +11,36 @@ const uploadPublicKey = (req, res) => {
     return res.status(400).send('Chave pública não fornecida');
   }
 
-  // Salvar a chave pública em memória ou em um arquivo
   publicKey = key;
-  fs.writeFileSync(path.join(__dirname, '../../publicKey.pem'), key);
 
+  // Salvar a chave pública no arquivo
+  const filePath = path.join(__dirname, '../../public_key.pem');
+  fs.writeFileSync(filePath, key);
+
+  console.log('Chave pública carregada e salva com sucesso:', key);
   res.status(200).send('Chave pública carregada com sucesso');
 };
 
-// Função para processar o fluxo
 const handleFlow = (req, res) => {
   const signature = req.headers['x-hub-signature-256'];
   const body = req.body;
+
+  // Carregar a chave pública do arquivo, se não estiver em memória
+  if (!publicKey) {
+    const filePath = path.join(__dirname, '../../public_key.pem');
+    if (fs.existsSync(filePath)) {
+      publicKey = fs.readFileSync(filePath, 'utf8');
+    } else {
+      return res.status(500).send('Chave pública não encontrada');
+    }
+  }
 
   if (!verifySignature(signature, body, publicKey)) {
     return res.status(401).send('Assinatura inválida');
   }
 
-  // Processar o fluxo aqui
   console.log('Fluxo recebido:', body);
   res.status(200).send('Fluxo processado com sucesso');
 };
 
-module.exports = { handleFlow, uploadPublicKey };
+module.exports = { uploadPublicKey, handleFlow };
