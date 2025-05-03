4cbf47141bf957b5cfbc36bd130d2b3436151797
RobsonOliiveiraa <robinhoramos19@gmail.com>
2025-05-03 04:12:12 +0000

    Assinatura Válida

--- generateSignature.js
+++ generateSignature.js
@@ -1,9 +1,20 @@
 const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
 
+// Corpo da requisição (deve ser exatamente o mesmo enviado no endpoint)
 const body = JSON.stringify({
   example: "dados do fluxo"
-}); // Substitua pelo corpo da requisição
-const secret = "sua-chave-privada"; // Substitua pela chave privada correspondente à chave pública
+});
 
-const hash = crypto.createHmac('sha256', secret).update(body).digest('hex');
-console.log(`x-hub-signature-256: sha256=${hash}`);
+// Carregar a chave privada do arquivo
+const privateKeyPath = path.join(__dirname, 'private_key.pem');
+const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
+
+// Criar a assinatura
+const sign = crypto.createSign('sha256');
+sign.update(body);
+sign.end();
+
+const signature = sign.sign(privateKey, 'hex');
+console.log(`x-hub-signature-256: sha256=${signature}`);
