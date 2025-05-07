8c853f5131925069013f228e6eab2fd939a173c4
RobsonOliiveiraa <robinhoramos19@gmail.com>
2025-05-07 20:07:03 +0000

    b8

--- appsecret_proof.js
+++ appsecret_proof.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
 
 const appSecret = '71151079bb41abd69608ae0b28f2f815'; // Substitua pelo seu APP_SECRET
-const accessToken = 'EAATK06J9ZAV8BO8NAqmgEUjGKIE0yZCp5ZCnE00IcQdZA0HnbPUuZAwVzzO0I2kcCWD0ZBZCNKI81H5iBQzEKKA2LBJXqI1YHvOtFLaubMwgoSOZAKoffohsHrO6h7ZBsSzCpkCRoclvG2YGNTCABBkfy5CP5ZAeaohZCuorKMOKgS3uK8O2qcPU3vMUTfT0adaLqTsGrZBglRE7ZACHaBwDFT5hju4Qcuc1JvAZDZD';
+const accessToken = 'EAATK06J9ZAV8BO8NAqmgEUjGKIE0yZEAATK06J9ZAV8BOzBewfh5dlHToZATZCqnA5Th1xQ6gObd9wiytYYGnz1ZA7OX8uSQl18xovw9WOoSr8lkpQZChOTGOqw88aty3ZAc9B2c1QTNnKAj5xoucdclZBdGw2GEfJDefUwi9zft6kIZCW32jYkrKCZCXBFrdtmIJZARWyoNx2BFmTCY77JrW8ZBfZAyZAhH8nu0v0d1qJOZBqlyoukNx2EQGBPd7JxlCqOcZD';
 
 const appsecretProof = crypto
   .createHmac('sha256', appSecret)
