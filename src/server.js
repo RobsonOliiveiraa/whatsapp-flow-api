58102139029f207d6dd31f72150c67eab6f28edf
RobsonOliiveiraa <robinhoramos19@gmail.com>
2025-05-08 17:27:49 +0000

    acessnew

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
+
 const express = require('express');
 const flowRoutes = require('./routes/flowRoutes');
-require('dotenv').config();
+
+
+console.log('APP_SECRET:', process.env.APP_SECRET ? 'Carregado com sucesso' : 'Não encontrado');
+console.log('FACEBOOK_ACCESS_TOKEN:', process.env.FACEBOOK_ACCESS_TOKEN ? 'Carregado com sucesso' : 'Não encontrado');
+console.log('FACEBOOK_BUSINESS_ID:', process.env.FACEBOOK_BUSINESS_ID ? 'Carregado com sucesso' : 'Não encontrado');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
