502f54efbaf6abaa2f6ea0fdf0f7591045b36720
RobsonOliiveiraa <robinhoramos19@gmail.com>
2025-05-08 00:04:29 +0000

    48f29064

--- src/routes/flowRoutes.js
+++ src/routes/flowRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const { validateWebhook, handleFlow, sendPublicKeyToFacebook } = require('../controllers/flowController');
 
 // Rota para validação do webhook
-router.get('/flow', validateWebhook);
+router.get('/flows/webhook', validateWebhook);
 
 // Rota para lidar com o fluxo
-router.post('/flow', handleFlow);
+router.post('/flows/webhook', handleFlow);
 
 // Rota para enviar a chave pública ao Facebook
 router.post('/sendPublicKey', sendPublicKeyToFacebook);
